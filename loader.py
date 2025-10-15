6f9bd9969a1bbc4d0546c1c325594bca3ebd0de3
Haile Asaye <haileasaye51@gmail.com>
2025-10-15 21:29:49 +0300

    loader is created to make the bot conn & dispatcher separate of concern

---
+++ loader.py
@@ -0,0 +1,10 @@
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
+from config import BOT_TOKEN
+
+bot = Bot(
+    token=BOT_TOKEN,
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+)
+dp = Dispatcher()
