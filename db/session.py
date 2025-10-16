b98b6c029fde4386d0809819708862bf76aa1239
Haile Asaye <haileasaye51@gmail.com>
2025-10-16 23:57:03 +0300

    the session.py is refactored to make it ready for prod

--- db/session.py
+++ db/session.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
 from config import DATABASE_URL
+
 engine = create_engine(DATABASE_URL)
 
 Sessionlocal  = sessionmaker(autoflush=False , autocommit=False , bind=engine)
